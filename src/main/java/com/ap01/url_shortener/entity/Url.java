b3048c8534ea178d19d6084b949167eb6e8679d4
ARUNKUMAR2906 <arunkumar290601@gmail.com>
2026-08-18 21:13:57 +0530

    day1 complete

---
+++ src/main/java/com/ap01/url_shortener/entity/Url.java
@@ -0,0 +1,15 @@
+package com.ap01.url_shortener.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Url {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String originalUrl;
+    private String shortCode;
+}
---
+++ src/main/java/com/ap01/url_shortener/entity/user.java
@@ -0,0 +1,4 @@
+package com.ap01.url_shortener.entity;
+
+public class user {
+}
